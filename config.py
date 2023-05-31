93a0ce6e9b9a1a1f99b5f17731675f2fcfa0023c
TOXIC-BOY-OP <134930342+TOXIC-BOY-OP@users.noreply.github.com>
2023-05-31 09:37:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ load_dotenv()
 admins = {}
 SESSION_NAME = getenv("SESSION_NAME", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", "1829202"))
-API_ID = int(getenv("API_ID", "8186557")) #oP
-API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
+OWNER_ID = int(getenv("OWNER_ID", "6109531260"))
+API_ID = int(getenv("API_ID", "25374274")) #oP
+API_HASH = getenv("API_HASH", "d0012f0876e6f9308eec2f474e7b9273")
